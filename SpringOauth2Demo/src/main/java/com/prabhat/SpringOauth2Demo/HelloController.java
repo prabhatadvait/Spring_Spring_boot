9aff3c1538ca7cbf7dd682efcc1426e01f2f5091
Prabhat Kumar <116169998+prabhatadvait@users.noreply.github.com>
2025-06-10 19:22:07 +0530

    Add files via upload

---
+++ SpringOauth2Demo/src/main/java/com/prabhat/SpringOauth2Demo/HelloController.java
@@ -0,0 +1,13 @@
+package com.prabhat.SpringOauth2Demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String greet(){
+        return "Welcome to the Prabhat's World";
+    }
+}
---
+++ SpringOauth2Demo/src/test/java/com/prabhat/SpringOauth2Demo/SpringOauth2DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prabhat.SpringOauth2Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauth2DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
